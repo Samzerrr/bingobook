6e3ce14b206afa9592f8af31c1578caa8e241fba
Samzerrr <88988090+Samzerrr@users.noreply.github.com>
2025-03-11 06:48:14 +0100

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bingo_book";
+$dbname = "bingo_book_fsc";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
